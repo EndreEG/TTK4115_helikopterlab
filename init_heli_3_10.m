c57748e03a9b373f72927e2b1a47e055de10260a
EndreEG <157694311+EndreEG@users.noreply.github.com>
2024-09-05 11:31:50 +0200

    Add files via upload

---
+++ init_heli_3_10.m
@@ -0,0 +1,34 @@
+% FOR HELICOPTER NR 3-10
+% This file contains the initialization for the helicopter assignment in
+% the course TTK4115. Run this file before you execute QuaRC_ -> Build 
+% to build the file heli_q8.mdl.
+
+% Oppdatert høsten 2006 av Jostein Bakkeheim
+% Oppdatert høsten 2008 av Arnfinn Aas Eielsen
+% Oppdatert høsten 2009 av Jonathan Ronen
+% Updated fall 2010, Dominik Breu
+% Updated fall 2013, Mark Haring
+% Updated spring 2015, Mark Haring
+
+
+%%%%%%%%%%% Calibration of the encoder and the hardware for the specific
+%%%%%%%%%%% helicopter
+Joystick_gain_x = 1;
+Joystick_gain_y = -1;
+
+
+%%%%%%%%%%% Physical constants
+g = 9.81; % gravitational constant [m/s^2]
+l_c = 0.46; % distance elevation axis to counterweight [m]
+l_h = 0.66; % distance elevation axis to helicopter head [m]
+l_p = 0.175; % distance pitch axis to motor [m]
+m_c = 1.92; % Counterweight mass [kg]
+m_p = 0.72; % Motor mass [kg]
+e_offset= 0.5;
+k_f = 0.1345;
+vs_0 = 8;
+k_1 = k_f/(2*m_p*l_p);
+lambda_1= -5;
+lambda_2= -5;
+k_p = (lambda_1*lambda_2)/k_1;
+k_d = (-lambda_1-lambda_2)/k_1;
