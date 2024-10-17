0f66bb28bb0e322e164561b1b1e2f02330ce254a
EndreEG <157694311+EndreEG@users.noreply.github.com>
2024-10-17 13:11:46 +0200

    Add files via upload

--- init_heli_3_10.m
+++ init_heli_3_10.m
@@ -1,3 +1,4 @@
+
 % FOR HELICOPTER NR 3-10
 % This file contains the initialization for the helicopter assignment in
 % the course TTK4115. Run this file before you execute QuaRC_ -> Build 
@@ -34,19 +35,28 @@ k_p = (lambda_1*lambda_2)/k_1;
 k_d = (-lambda_1-lambda_2)/k_1;
 A=[0,1,0,0,0;0,0,0,0,0;0,0,0,0,0;-1,0,0,0,0;0,0,-1,0,0];
 l_1=k_f*l_p;
-k_1=l_1/(2*m_p*l_p^2);
+l_2=g*(m_c*l_c-2*m_p*l_h);
 l_3=l_h*k_f;
+l_4=k_f*l_h;
 j_e=m_c*l_c^2+2*m_p*l_h^2;
+k_1=l_1/(2*m_p*l_p^2);
 k_2=l_3/j_e;
+k_3=(l_4*(l_2/l_3))/(m_c*l_c^2+2*m_p*(l_h^2+l_p^2));
 B=[0,0;0,k_1;k_2,0;0,0;0,0];
-q_1=10;
-q_2=10;
-q_3=10;
-q_4=10;
+q_1=50;
+q_2=25;
+q_3=30;
+q_4=20;
 q_5=10;
 Q_LQR=[q_1,0,0,0,0;0,q_2,0,0,0;0,0,q_3,0,0;0,0,0,q_4,0;0,0,0,0,q_5];
-r_1=100;
-r_2=100;
+r_1=1;
+r_2=1;
 R_LQR=[r_1,0;0,r_2];
 K=lqr(A,B,Q_LQR,R_LQR);
 F=[K(1,1),K(1,3);K(2,1),K(2,3)];
+A_lue=[0,1,0,0,0;0,0,0,0,0;0,0,0,1,0;0,0,0,0,0;k_3,0,0,0,0];
+PORT=5;
+B_lue=[0,0;0,k_1;0,0;k_2,0;0,0];
+C=diag([1 1 1 1 1]);
+p=[-2,-2.5,-3,-3.5,-4];
+L=place(A_lue',C',3*p)';
